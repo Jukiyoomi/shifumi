19c157d95d6c418585d5e959f970b2f0b1feb03f
Jukiyoomi <jnguessan2003@gmail.com>
2022-03-31 16:13:50 +0200

    added font, images folder, colors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import '../style/components/App.css'
+import scissor from '../public/scissor.svg'
 
 function App() {
-
-  return (
-      <h1 className="text-center font-bold text-2xl text-red-500">Hello World</h1>
-  );
+    return <h1 className="text-center font-bold text-2xl uppercase logo">Shifumi</h1>
 }
 
 export default App
